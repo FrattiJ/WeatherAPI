443643579d447d436bb378a4932f5c6f316ad5c8
Jacob Fratti <JacobRFratti@gmail.com>
2023-10-04 14:03:14 -0400

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WeatherAPI
 Week 6 Challenge of a weather api
+
+## Preview
+![](./assets/Weather%20App.gif)
+
+## Deployable
+https://frattij.github.io/WeatherAPI
--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,44 @@
-const apiKey = '119f2d92128a6ab118c2d0bd14488ce8';
-const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+var apiKey = '119f2d92128a6ab118c2d0bd14488ce8';
+var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
 // Function to fetch weather data
 async function fetchWeatherData(city) {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
-    const response = await fetch(apiUrl);
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
+    var response = await fetch(apiUrl);
     return response.json();
 }
 
 // Function to display current weather
 function displayCurrentWeather(data) {
-    const currentWeatherElement = document.getElementById('currentWeather');
+    var currentWeatherElement = document.getElementById('currentWeather');
 
     // Clear previous weather data
     currentWeatherElement.innerHTML = '';
 
     // Create elements to display current weather data
-    const cityNameElement = document.createElement('div');
+    var cityNameElement = document.createElement('div');
     cityNameElement.classList.add('city-name');
     cityNameElement.textContent = data.name;
 
-    const date = new Date(data.dt * 1000); // Convert timestamp to milliseconds
-    const dateElement = document.createElement('div');
+    var date = new Date(data.dt * 1000); // Convert timestamp to milliseconds
+    var dateElement = document.createElement('div');
     dateElement.classList.add('date');
     dateElement.textContent = date.toDateString();
 
-    const weatherIconElement = document.createElement('img');
+    var weatherIconElement = document.createElement('img');
     weatherIconElement.classList.add('weather-icon');
     weatherIconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     weatherIconElement.alt = data.weather[0].description;
 
-    const temperatureElement = document.createElement('div');
+    var temperatureElement = document.createElement('div');
     temperatureElement.classList.add('temperature');
     temperatureElement.textContent = `Temperature: ${data.main.temp}°C`;
 
-    const humidityElement = document.createElement('div');
+    var humidityElement = document.createElement('div');
     humidityElement.classList.add('humidity');
     humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
 
-    const windSpeedElement = document.createElement('div');
+    var windSpeedElement = document.createElement('div');
     windSpeedElement.classList.add('wind-speed');
     windSpeedElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
 
@@ -54,47 +54,47 @@ function displayCurrentWeather(data) {
 
 // Function to fetch and display 5-day forecast
 async function fetchAndDisplayForecast(city) {
-    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    const response = await fetch(forecastApiUrl);
-    const forecastData = await response.json();
+    var forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    var response = await fetch(forecastApiUrl);
+    var forecastData = await response.json();
 
     // Check if the forecast data is available
     if (forecastData.cod === "200") {
-        const forecastList = forecastData.list;
+        var forecastList = forecastData.list;
 
         // Clear previous forecast data
         document.getElementById('forecast').innerHTML = '';
 
         // Loop through the forecast data for the next 5 days (assuming data is available for every 3 hours)
         for (let i = 0; i < forecastList.length; i += 8) {
-            const forecast = forecastList[i];
+            var forecast = forecastList[i];
 
             // Create a container for each day's forecast
-            const forecastContainer = document.createElement('div');
+            var forecastContainer = document.createElement('div');
             forecastContainer.classList.add('forecast-item');
 
             // Extract and format the date
-            const date = new Date(forecast.dt * 1000); // Convert timestamp to milliseconds
-            const dateString = date.toDateString();
+            var date = new Date(forecast.dt * 1000); // Convert timestamp to milliseconds
+            var dateString = date.toDateString();
 
             // Create elements to display forecast data
-            const dateElement = document.createElement('div');
+            var dateElement = document.createElement('div');
             dateElement.classList.add('forecast-date');
             dateElement.textContent = dateString;
 
-            const iconElement = document.createElement('img');
+            var iconElement = document.createElement('img');
             iconElement.classList.add('forecast-icon');
             iconElement.src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
 
-            const tempElement = document.createElement('div');
+            var tempElement = document.createElement('div');
             tempElement.classList.add('forecast-temp');
             tempElement.textContent = `Temp: ${forecast.main.temp}°C`;
 
-            const windElement = document.createElement('div');
+            var windElement = document.createElement('div');
             windElement.classList.add('forecast-wind');
             windElement.textContent = `Wind: ${forecast.wind.speed} m/s`;
 
-            const humidityElement = document.createElement('div');
+            var humidityElement = document.createElement('div');
             humidityElement.classList.add('forecast-humidity');
             humidityElement.textContent = `Humidity: ${forecast.main.humidity}%`;
 
@@ -115,7 +115,7 @@ async function fetchAndDisplayForecast(city) {
 
 // Function to update the search history sidebar
 function updateSearchHistory() {
-    const city = document.getElementById('cityInput').value.trim();
+    var city = document.getElementById('cityInput').value.trim();
 
     if (city) {
         // Append new city to the search history
@@ -125,11 +125,11 @@ function updateSearchHistory() {
         localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 
         // Update the search history section
-        const searchHistoryElement = document.getElementById('searchHistory');
+        var searchHistoryElement = document.getElementById('searchHistory');
         searchHistoryElement.innerHTML = '<h2>Search History</h2>';
         
-        for (const item of searchHistory) {
-            const listItem = document.createElement('div');
+        for (var item of searchHistory) {
+            var listItem = document.createElement('div');
             listItem.classList.add('history-item');
             listItem.textContent = item;
             searchHistoryElement.appendChild(listItem);
@@ -137,14 +137,13 @@ function updateSearchHistory() {
     }
 }
 
-
 // Event listener for the search button
 document.getElementById('searchBtn').addEventListener('click', async () => {
-    const city = document.getElementById('cityInput').value.trim();
+    var city = document.getElementById('cityInput').value.trim();
 
     if (city) {
         // Fetch current weather data
-        const currentWeatherData = await fetchWeatherData(city);
+        var currentWeatherData = await fetchWeatherData(city);
 
         if (currentWeatherData.cod === 200) {
             // Display current weather data
@@ -164,11 +163,11 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
 // Event listener for clicking on a city in the search history
 document.getElementById('searchHistory').addEventListener('click', async (event) => {
     if (event.target.classList.contains('history-item')) {
-        const city = event.target.textContent;
+        var city = event.target.textContent;
 
         try {
             // Fetch current weather data
-            const currentWeatherData = await fetchCurrentWeather(city);
+            var currentWeatherData = await fetchCurrentWeather(city);
 
             if (currentWeatherData.cod === 200) {
                 // Display current weather data
@@ -188,11 +187,11 @@ document.getElementById('searchHistory').addEventListener('click', async (event)
 
 // Function to update the search history sidebar when the page loads
 function updateSearchHistoryOnLoad() {
-    const searchHistoryElement = document.getElementById('searchHistory');
+    var searchHistoryElement = document.getElementById('searchHistory');
     searchHistoryElement.innerHTML = '<h2>Search History</h2>';
     
-    for (const item of searchHistory) {
-        const listItem = document.createElement('div');
+    for (var item of searchHistory) {
+        var listItem = document.createElement('div');
         listItem.classList.add('history-item');
         listItem.textContent = item;
         searchHistoryElement.appendChild(listItem);
